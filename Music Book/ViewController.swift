06362d4033b550714b7ec4d035866c0ffb3287c8
Sergio Popov <sergio@sergeys-macbook-pro.local>
2018-04-24 17:46:27 +0300

    Initial Commit

---
+++ Music Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music Book
+//
+//  Created by Sergio on 24.04.2018.
+//  Copyright © 2018 Sergio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
