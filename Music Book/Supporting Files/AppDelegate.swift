c605dd7843f373923f2812fcdca285527fefaf08
Sergi <sergey.s.popov@icloud.com>
2018-05-22 15:20:42 +0300

    Supporting files update.

--- Music Book/Supporting Files/AppDelegate.swift
+++ Music Book/Supporting Files/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        /* Microphone permission request */
         let session = AVAudioSession.sharedInstance()
-        
         do {
             try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions.defaultToSpeaker)
             try session.setActive(true)
@@ -32,6 +32,74 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } catch let error {
             debugPrint(error.localizedDescription)
         }
+        
+        /* Splash screen animation */
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        self.window!.backgroundColor = UIColor.flatNavyBlueDark
+//        self.window!.makeKeyAndVisible()
+//
+//        // rootViewController from StoryBoard
+//        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//        let navigationController = mainStoryboard.instantiateViewController(withIdentifier: "mainViewController")
+//        self.window!.rootViewController = navigationController
+        
+//        // logo mask
+//        navigationController.view.layer.mask = CALayer()
+//        navigationController.view.layer.mask?.contents = UIImage(named: "Record Disabled.png")!.cgImage
+//        navigationController.view.layer.mask?.bounds = CGRect(x: 0, y: 0, width: 60, height: 60)
+//        navigationController.view.layer.mask?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+//        navigationController.view.layer.mask?.position = CGPoint(x: navigationController.view.frame.width / 2, y: navigationController.view.frame.height / 2)
+//
+//        // logo mask background view
+//        let maskBgView = UIView(frame: navigationController.view.frame)
+//        maskBgView.backgroundColor = UIColor.flatNavyBlueDark
+//        navigationController.view.addSubview(maskBgView)
+//        navigationController.view.bringSubview(toFront: maskBgView)
+//
+//        // logo mask animation
+//        let transformAnimation = CAKeyframeAnimation(keyPath: "bounds")
+//        transformAnimation.delegate = self as? CAAnimationDelegate
+//        transformAnimation.duration = 1
+//        transformAnimation.beginTime = CACurrentMediaTime() + 1 //add delay of 1 second
+//        let initalBounds = NSValue(cgRect: (navigationController.view.layer.mask?.bounds)!)
+//        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 50, height: 50))
+//        let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 2000, height: 2000))
+//        transformAnimation.values = [initalBounds, secondBounds, finalBounds]
+//        transformAnimation.keyTimes = [0, 0.5, 1]
+//        transformAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+//        transformAnimation.isRemovedOnCompletion = false
+//        transformAnimation.fillMode = kCAFillModeForwards
+//        navigationController.view.layer.mask?.add(transformAnimation, forKey: "bounds")
+//
+//        // logo mask background view animation
+//        UIView.animate(withDuration: 0.1,
+//                                   delay: 1.35,
+//                                   options: UIViewAnimationOptions.curveEaseIn,
+//                                   animations: {
+//                                    maskBgView.alpha = 0.0
+//        },
+//                                   completion: { finished in
+//                                    maskBgView.removeFromSuperview()
+//        })
+ 
+        // root view animation
+//        UIView.animate(withDuration: 0.25,
+//                                   delay: 1.3,
+//                                   options: [],
+//                                   animations: {
+//                                    self.window!.rootViewController!.view.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+//        },
+//                                   completion: { finished in
+//                                    UIView.animate(withDuration: 0.3,
+//                                                               delay: 0.0,
+//                                                               options: [.curveEaseInOut],
+//                                                               animations: {
+//                                                                self.window!.rootViewController!.view.transform = .identity
+//                                    },
+//                                                               completion: nil
+//                                    )
+//        })
+        
         return true
     }
 
