fcc01c0a35ca86a7a91b3b839b7dda131238443c
Sergi <sergey.s.popov@icloud.com>
2018-05-25 01:57:39 +0300

    Helper classes.

---
+++ Music Book/DataStorage.swift
@@ -0,0 +1,15 @@
+//
+//  DataStorage.swift
+//  Music Book
+//
+//  Created by Sergio on 03.05.2018.
+//  Copyright © 2018 Sergi Popov. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class DataStorage {
+    static var applicationHasMicrophoneAccess = false
+    static var audios: [AKAudioFile] = []
+}
---
+++ Music Book/State.swift
@@ -0,0 +1,16 @@
+//
+//  State.swift
+//  Music Book
+//
+//  Created by Sergio on 21/5/18.
+//  Copyright © 2018 Sergio. All rights reserved.
+//
+
+enum AKState {
+    case automaticRecognition
+    case readyToRecord
+    case recording
+    case readyToPlay
+    case playing
+    case noAccess
+}
