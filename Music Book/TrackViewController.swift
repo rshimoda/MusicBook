c7466056a4610cf04c7fc6f84c0335185a94483b
Sergi <sergey.s.popov@icloud.com>
2018-06-01 16:16:11 +0300

    Track View.

---
+++ Music Book/TrackViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrackViewController.swift
+//  Music Book
+//
+//  Created by Sergio on 28/05/18.
+//  Copyright © 2018 Sergio. All rights reserved.
+//
+
+import UIKit
+
+class TrackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        navigationController?.setToolbarHidden(false, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+        navigationController?.setToolbarHidden(true, animated: animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
